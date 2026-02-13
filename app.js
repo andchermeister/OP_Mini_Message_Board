82a222155ab6850f29485ec2579ca13b7f45609c
Andrii Chernikov <99753757+andchermeister@users.noreply.github.com>
2026-02-13 10:15:31 +0000

    Added form for the message, updated the router by adding post request handler.

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const newMessageRouter = require("./routes/newMessageRouter");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", indexRouter);
-app.get("/new", newMessageRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.statuCode || 500).send(err.message);
--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -2,8 +2,15 @@ const { Router } = require("express");
 
 const newMessageRouter = Router();
 
-newMessageRouter.get("/new", (req, res) =>
-  res.render("newMessage", { message: "that is a new message page" }),
-);
+newMessageRouter.get("/", (req, res) => res.render("form"));
+
+newMessageRouter.post("/", (req, res) => {
+  const { author, message } = req.body;
+
+  console.log(author);
+  console.log(message);
+
+  res.redirect("/");
+});
 
 module.exports = newMessageRouter;
