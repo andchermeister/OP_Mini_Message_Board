fb009b982dfe5d6dd1482ef543591ae36f60f03e
Andrii Chernikov <99753757+andchermeister@users.noreply.github.com>
2026-03-02 20:14:08 +0000

    Connected to the posgresql on render platform.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,18 +1,20 @@
 const messageModel = require("../models/messageModel");
 const CustomNotFoundError = require("../errors/CustomNotFoundError");
+const db = require("../models/queries");
 
-function getMessageById(req, res) {
+async function getMessageById(req, res) {
   const { messageId } = req.params;
   let message;
   try {
     message = messageModel.getMessageById(messageId);
+    await db.testConnection();
 
     if (!message) {
       throw new CustomNotFoundError("Message not found");
     }
   } catch (error) {
     console.error("Error retrieving author:", error);
-    res.status(500).send("Internal Server Error");
+    return res.status(500).send("Internal Server Error");
   }
 
   res.render("message", { message });
---
+++ models/queries.js
@@ -0,0 +1,13 @@
+const pool = require("./pool");
+
+async function testConnection() {
+  try {
+    const { rows } = await pool.query("SELECT NOW()");
+    console.log("DB Connection OK - current time:", rows[0].now);
+  } catch (error) {
+    console.error("DB Connection FAILED:", error);
+    throw error;
+  }
+}
+
+module.exports = { testConnection };
